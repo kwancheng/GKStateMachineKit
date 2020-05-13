14ac1e6ab31a3d1e4c797a120a0f1f90cf4cc13e
Ghengis Kuan <gk@ghengiskuan.com>
2020-05-13 12:28:55 -0400

    Initial Commit - Sample App

---
+++ Basic Sample App/Basic Sample App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Basic Sample App
+//
+//  Created by Ghengis Kuan on 5/13/20.
+//  Copyright © 2020 Ghengis Kuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
